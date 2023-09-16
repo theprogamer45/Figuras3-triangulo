9d5e5d07187b17d99e8ea84b28b303382e4c3912
theprogamer45 <juanpgh21@gmail.com>
2023-09-15 23:39:10 -0500

    Circulo

---
+++ src/poo/pkg3/ejercicio/figuras/geometrica/triangulo/rectangulo/POO3EjercicioFigurasGeometricaTrianguloRectangulo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo.pkg3.ejercicio.figuras.geometrica.triangulo.rectangulo;
+
+/**
+ *
+ * @author juanp
+ */
+public class POO3EjercicioFigurasGeometricaTrianguloRectangulo {
+
+    
+    public static void main(String[] args) {
+        Triangulo form = new Triangulo();
+        form.setVisible(true);
+    }
+    
+}
